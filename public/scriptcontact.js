fcf031254868222ccc3719cd9ee4d5b835e7b67a
Prasoon321 <116250058+Prasoon321@users.noreply.github.com>
2024-09-08 21:26:07 +0530

    Create scriptcontact.js

---
+++ public/scriptcontact.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    console.log("hello")
+    $("#handleform").submit(function (e) {
+        e.preventDefault()
+        const name = $('#name').val()
+        const email = $('#Email').val()
+        const contact = $('#Phone').val()
+        const purpose = $('#Message').val()
+
+
+        console.log(name)
+        console.log(email)
+        console.log(contact)
+        console.log(purpose)
+        if (!name) {
+            alert("Please fill the name first")
+            return
+        }
+        if (!contact) {
+            alert("Please fill the contact first")
+            return
+        }
+        if (!purpose) {
+            alert("Mentioning your purpose is good for your query")
+            return
+        }
+        dataobj = {
+            name: name,
+            email: email,
+            contact: contact,
+            purpose: purpose
+
+        }
+        console.log(dataobj)
+        $("#submitbutton").text("Request Processing....")
+        $.ajax({
+            url: 'http://localhost:4000/form/send',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(dataobj),
+            success: function (response) {
+                $("#submitbutton").text("Request Submited")
+                $('#name').val(' ')
+                $('#Email').val('')
+                $('#Phone').val(' ')
+                $('#Message').val(' ')
+            },
+            error: function (error) {
+                $("#submitbutton").text("Please Try Again")
+            }
+        })
+    })
+
+})
